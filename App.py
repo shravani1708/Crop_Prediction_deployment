de763a60e0a9b0a083534be33124904a80939fae
shravani1708 <113713917+shravani1708@users.noreply.github.com>
2024-10-31 00:44:27 +0530

    Update App.py

--- App.py
+++ App.py
@@ -91,4 +91,4 @@ def get_weather():
         return jsonify({'error': 'Weather data not available for the given city.'}), 404
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
