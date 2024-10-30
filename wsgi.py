7db5979a7939a7b4e656baaec7c86d0a1e0144ec
shravani1708 <113713917+shravani1708@users.noreply.github.com>
2024-10-31 00:44:59 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,12 +1,4 @@
-import sys
-import os
-
-# Add the project directory to the sys.path
-project_home = os.path.dirname(os.path.abspath(__file__))
-if project_home not in sys.path:
-    sys.path.append(project_home)
-
-from app import app as application  # Import the Flask app from app.py
+from app import app
 
 if __name__ == "__main__":
-    application.run()
+    app.run()
